f26539b7a57b5231101d36b454bb8428d0dd6655
erd.colak <erd.colak0@gmail.com>
2025-01-14 22:24:47 +0300

    Change Photon to Mirror 2

---
+++ Assets/Scripts/Platforms.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+
+public class Platforms : MonoBehaviour
+{
+    [Header("Settings")]
+    public float jumpPower;
+
+    public float destroyFloat;
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+        if (rb != null && rb.linearVelocity.y <= 0)
+        {
+            rb.linearVelocity = new Vector2(0, jumpPower);
+            Destroy(this.gameObject, destroyFloat);
+        }
+    }
+}
